5bc5c11599bf361e516a4205c6291e0dc043e7d8
Nadya_Fedotova <nadya.fedotova01@mail.ru>
2021-11-14 02:04:07 +0300

    echo-server
    add files

---
+++ echo_server/client.py
@@ -0,0 +1,36 @@
+#import
+import socket
+import time
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+port = int(input('Type here a number of port you want to connect (default is 2000)\n'))
+name = input('Type here a hostname you want to connect (default is localhost)\n')
+try:
+    client.connect((name, port))
+except (ConnectionRefusedError, socket.gaierror):
+    print("Connection with this IP or port doesn't available now.\nWould like to set defaults? Y\\N")
+    if input() == "Y":
+        #Соединение с сервером
+        client.connect(('127.0.0.1', 2000))
+    else:
+        print('OK. Try again...')
+        exit()
+print(client.recv(2048).decode('utf-8'))
+try:
+        while True:
+            #Отправка данных серверу;
+            client.send(input('Enter text to server:\n').encode('utf-8'))
+            data = client.recv(2048).decode('utf-8')
+            time.sleep(3)
+            #Разрыв соединения с сервером;
+            if data.lower() == 'exit':
+                print('>>>\nExit command received\nBye!\n<<<')
+                client.send('User disconnected\n'.encode('utf-8'))
+                client.shutdown(socket.SHUT_WR)
+                exit()
+            #Прием данных от сервера.
+            if data:
+                print('\nData from server:')
+                print(data, end='\n\n')
+except KeyboardInterrupt:
+    client.shutdown(socket.SHUT_WR)
---
+++ echo_server/server.py
@@ -0,0 +1,48 @@
+#import
+import socket
+import time
+#Запуск сервера
+print('Starting server...')
+PORT = 2000
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+server.bind(('127.0.0.1', PORT))
+
+try:
+    while True:
+        server.listen(1)
+        #подключение клента
+        client_socket, address = server.accept()
+        print(f'User with ip {address} connected')
+        
+        client_socket.send('You are connected!'.encode('utf-8'))
+        while True:
+            try:
+                data = client_socket.recv(2048).decode('utf-8')
+            except OSError:
+                data = None
+
+            time.sleep(3)
+            if data:
+                #получение данных от клиента
+                print('\nReceiving data from client:')
+                print(data)
+            msg = input('\nEnter text to client:\n').encode('utf-8')
+            #отправка сообщений клиенту
+            client_socket.send(msg)
+            if msg == 'exit'.encode('utf-8'):
+                time.sleep(2)
+                #отключение клиента
+                print('exit command found')
+                server.shutdown(socket.SHUT_WR)
+                server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                server.bind(('127.0.0.1', 2000))
+                break
+#остановка сервера                
+except KeyboardInterrupt:
+    print('Server is shutting down')
+    server.shutdown(socket.SHUT_WR)
+except BrokenPipeError:
+    print('Server doesnt have active users...\nShutting down!')
+    server.shutdown(socket.SHUT_WR)
