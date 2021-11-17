72fcec516619be066b5a27b937b7c86d04705728
Nadya_Fedotova <nadya.fedotova01@mail.ru>
2021-11-17 17:33:47 +0300

    работает

--- file_manager/file_manager_functions.py
+++ file_manager/file_manager_functions.py
@@ -2,37 +2,56 @@ import os
 #from settings import work_directory as wd
 wd= "C:\\Users\\Asus\\Desktop\\Test\\" #Папка для тестирования работы функций 
 
-def dir_exists(new_path):
+def __dir_exists(new_path):
     if os.path.isdir(new_path):
         return True
     else:
         return False
 
-def enter_name(s=""):
+def __enter_name(s=""):
     s="Введите имя"+s+": "
     name=input(s)
     while name=="":
         print("Пустая строка не может быть именем")
         name=input(s)
     return name
-    
+
+def __valid_folder(folder_name):
+    if "\\" not in folder_name:
+        folder_name=os.getcwd()+"\\"+folder_name
+    elif os.getcwd()+"\\" in wd+folder_name:
+        folder_name=os.getcwd()+"\\"+folder_name
+    else:
+        Exception
+
+    #print(folder_name)
+    assert (wd in folder_name), "Не рабочая директория"
+    return folder_name
+
+
+
 def new_folder():#Создание папки (с указанием имени);
-    folder_name= wd+enter_name()
-    if not dir_exists(folder_name):
+    folder_name = __enter_name()
+    folder_name = __valid_folder(folder_name)
+    if not __dir_exists(folder_name):
         os.mkdir(folder_name)
         return True
     return False
 
 def del_folder():#Удаление папки по имени;
-    folder_name=wd+enter_name()
-    if dir_exists(folder_name):
+    folder_name=__enter_name()
+    folder_name = __valid_folder(folder_name)
+
+    if __dir_exists(folder_name):
         os.rmdir(folder_name) 
         return True
     return False
 
 def goto_folder():#переход по имени
-    folder_name=wd+enter_name()
-    if dir_exists(folder_name):
+    folder_name=__enter_name()
+    folder_name = __valid_folder(folder_name)
+
+    if __dir_exists(folder_name):
         os.chdir(folder_name) 
         return True
     return False
@@ -44,7 +63,8 @@ def goup_folder():#на уровень верх
     return False    
 
 def create_empty_file():#создание пустого файла
-    file_name = enter_name()
+    file_name = __enter_name()
+
     if ".txt" not in file_name:
         file_name+=".txt"
     f=open(file_name, "w")
@@ -53,7 +73,7 @@ def create_empty_file():#создание пустого файла
     #return False    
 
 def add_text_file():    #Запись текста в файл;
-    file_name = enter_name()
+    file_name = __enter_name()
     if ".txt" not in file_name:
         file_name+=".txt"
     with open(file_name, "a") as f:
@@ -63,15 +83,15 @@ def add_text_file():    #Запись текста в файл;
             msg=input("Введите строку, для окончания записи введите 0: ")+"\n"
 
 def viewing_text_file():#Просмотр содержимого текстового файла    
-    file_name = enter_name()
+    file_name = __enter_name()
     if ".txt" not in file_name:
         file_name+=".txt"
     with open(file_name, "r") as f:
         for line in f:
             print(line[:-1])
 
-def delete_file():#удаление файла
-    file_name = enter_name()
+def del_file():#удаление файла
+    file_name = __enter_name()
     if ".txt" not in file_name:
         file_name+=".txt"
     try:
@@ -79,20 +99,28 @@ def delete_file():#удаление файла
     except:
         print("не удалось")
 
-#ДОДЕОЛАТЬ
 def copying_file():#Копирует один файл
     try:
-        file_name = enter_name(" копируемого файла")
+        file_name = __enter_name(" копируемого файла")
         if ".txt" not in file_name:
             file_name+=".txt"
-        folder_name=wd+enter_name()
+        print("point1")
+        folder_name=__enter_name()
+        folder_name = __valid_folder(folder_name)
+        print("point2")
         file_text=""
         with open(file_name, "r") as f:
             file_text=f.read()
-        if dir_exists(folder_name):
+            print("point3")
+            f.close()
+        if __dir_exists(folder_name):
             os.chdir(folder_name)
+            print("point4")
             with open(file_name, "w") as f:
-                file_text=f.write()
+                print("point5")
+                f.write(file_text)
+                print("point6")
+                f.close()
     except:
         print("error")    
         
@@ -101,20 +129,25 @@ def copying_file():#Копирует один файл
 
 #Перемещение
 def move_file():
+    wd_now=os.getcwd()
     try:
-        file_name = enter_name(" перемещаемого файла")
+        file_name = __enter_name(" перемещаемого файла")
         if ".txt" not in file_name:
             file_name+=".txt"
-        folder_name=wd+enter_name()
+        folder_name=__enter_name()
+        folder_name = __valid_folder(folder_name)
+
         file_text=""
         with open(file_name, "r") as f:
             file_text=f.read()
-        delete_file()
-        if dir_exists(folder_name):
+            f.close()
+        
+        if __dir_exists(folder_name):
             os.chdir(folder_name)
             with open(file_name, "w") as f:
-                file_text=f.write()
-        
+                f.write(file_text)
+            os.chdir(wd_now)
+        os.remove(file_name)
     except:
         print("error")    
         
@@ -122,10 +155,10 @@ def move_file():
     return False
 
 def rename_file():
-    old_file_name = enter_name()
+    old_file_name = __enter_name()
     if ".txt" not in old_file_name:
         old_file_name+=".txt"
-    new_file_name = enter_name()
+    new_file_name = __enter_name()
     if ".txt" not in new_file_name:
         new_file_name+=".txt"
     os.rename(old_file_name, new_file_name)
--- file_manager/file_manager_interface.py
+++ file_manager/file_manager_interface.py
@@ -1,63 +1,40 @@
 import file_manager_functions as fmf
-from settings import work_directory as wd
-
+#from settings import work_directory as wd
+wd= "C:\\Users\\Asus\\Desktop\\Test\\" #Папка для тестирования работы функций 
 
 session = 1
 actions=[
-    "exit", 
- "Создание папки", 
- "Удаление папки по имени", 
- "Заход в папку по имени", 
- "выход на уровень вверх", 
- "Создание пустых файлов с указанием имени", 
- "Запись текста в файл", 
- "Просмотр содержимого текстового файла", 
- "Удаление файлов по имени", 
- "Копирование файлов из одной папки в другую",
- "Перемещение файлов", 
- "Переименование файлов"]
-
-
-#actions={
-#   "0":["Выход"]
-#}
+    ["Выход", "До свидания!"], 
+    [ "Создание папки", fmf.new_folder], 
+    ["Удаление папки по имени",fmf.del_folder], 
+    ["Заход в папку по имени",fmf.goto_folder], 
+    ["выход на уровень вверх",fmf.goup_folder], 
+    ["Создание пустых файлов с указанием имени",fmf.create_empty_file], 
+    ["Запись текста в файл", fmf.add_text_file], 
+    ["Просмотр содержимого текстового файла",fmf.viewing_text_file], 
+    ["Удаление файлов по имени",fmf.del_file], 
+    ["Копирование файлов из одной папки в другую",fmf.copying_file],
+    ["Перемещение файлов",fmf.move_file], 
+    ["Переименование файлов",fmf.rename_file]
+]
 
 fmf.os.chdir(wd)    
+
 while session:
-    
     #вывод возможных действий
     print("Выберите действие: ")
-    for k,v in enumerate(actions):
-        print(k," - ",v)
-    #выбор действия
-    print(fmf.os.getcwd())#текущая директория
+    for i in range(len(actions)):
+        msg=" {0:2d} - {1}".format(i,actions[i][0])
+        print(msg)
+
+    print(fmf.os.getcwd())
     act=input("Введите номер действия: ")
-    if act=="0": #выход
+    if act=="0":
         print("До свидания!")
-        session=False  
-    elif act == "1": #"Создание папки"
-        if fmf.new_folder():
-            print("Папка успешно создана!")
-    elif act=="2": #Удаление папки по имени
-        if fmf.del_folder():
-            print("Папка успешно удалена!")
-    elif act=="3":#заход в папку по имени
-        fmf.goto_folder()
-    elif act=="4":#выход на уровень вверх
-        fmf.goup_folder()
-    elif act=="5":#Создание пустых файлов с указанием имени
-        fmf.create_empty_file()
-    elif act=="6":#Запись текста в файл
-        fmf.add_text_file()
-    elif act=="7":#Просмотр содержимого текстового файла
-        fmf.viewing_text_file()
-    elif act=="8":#Удаление файлов по имени
-        fmf.delete_file()
-    elif act=="9":#Копирование файлов из одной папки в другую
-        fmf.copying_file()
-    elif act=="10":#Перемещение файлов
-        pass
-    elif act=="11":#Переименование файлов
-        fmf.rename_file()
+        session=False
     else:
-        print("Этого действия пока нет") #если пользователь ввел что-то другое
+        try:
+            act=int(act)
+            actions[act][1]()
+        except:
+            print("Что-то пошло не так")
