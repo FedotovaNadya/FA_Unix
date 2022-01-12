84e4660e191b24e1af3377b0053b6ee415dacff7
Nadya_Fedotova <nadya.fedotova01@mail.ru>
2022-01-12 22:25:44 +0300

    web_server

--- 6_Web_server/server.py
+++ 6_Web_server/server.py
@@ -1,25 +1,60 @@
 import socket
+import logging
+import os
+
+
+wd = "6_Web_server\\6_wd\\"
+
+def log_print(st):
+    logging.info(st)
+    print(st)
+
+def genResp(dat):
+  d=dat.split()
+  print( d)
+  fn =d[1]
+  if fn[0] == "/":
+    fn=fn[1:]
+  if fn in os.listdir(wd):
+    print("qwer")
+  else:
+    print("jasdj")
+  return fn
+
+
+
+
+
 sock = socket.socket()
 try:
   sock.bind(('', 80))
 except OSError:
-  print("5555")
   sock.bind(('', 8080))
-except:
-  print("5555-except")
-  sock.bind(('', 5555)) 
+
 
 sock.listen(1000)
 conn, addr = sock.accept()
-print("Connected", addr)
-data = conn.recv(8192)
-msg = data.decode()
-print(msg) 
+log_print("Connected"+str(addr))
 
-resp = """HTTP/1.1 200 OK 
+while True:
+  try:
+    data = conn.recv(8192).decode('utf-8')
+  except OSError:
+    data = None
 
-Hello, webworld!"""
-conn.send(resp.encode())
+  if data:
+      #получение данных от клиента
+      log_print('\nReceiving data from client:')
+      log_print(data)
+      if data.lower() == 'exit':
+          log_print('here')
+          break
+      #отправка сообщений клиенту
+      file=genResp(data)
+      with open(wd+file, "r") as f:
+        resp = "HTTP/1.1 200 OK \n\n"+f.read()
+      print("he134re")
+      conn.send(resp.encode())
 
 conn.close()
 print("here")
